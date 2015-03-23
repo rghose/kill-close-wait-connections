ae49447913bb9536aeed844373b6dfc335c53652
Yorick Terweijden <terwey@gmail.com>
2015-03-23 19:18:20 +0100

    Update kill_close_wait.sh

--- kill_close_wait.sh
+++ kill_close_wait.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 netstat -tulnap | grep CLOSE_WAIT | awk '{print $4,$5}' |  sed 's/:/ /g' | while read line; do
-	perl /opt/scripts/killcw.pl $line;
+	perl ./kill_close_wait_connections.pl $line;
 done
-
